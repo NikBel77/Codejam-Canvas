448524cbd7f02bbfa1e98dc5fd9d3483a69fbcaa
Nikita Belyakov <nikita.belyakov.38@ya.ru>
2019-10-27 19:27:43 +0300

    feat: complite task

--- data/32x32.js
+++ data/32x32.js
@@ -1,3 +1,4 @@
+const matrix32 = 
 [
     [
       [
--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-let btnImg = document.querySelector('#img_lg');
-let canvas = document.querySelector('.canvas')
-let ctx = canvas.getContext("2d");
-let image = document.createElement('img');
-
 function drawPic () {
+    let ctx = document.querySelector('.canvas').getContext("2d");
+    let image = document.createElement('img');
     image.src = 'data/image.png';
     image.onload = function() {
-        ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+        ctx.drawImage(image, 0, 0, 512, 512)
     }
 }
 
-btnImg.addEventListener('click', function () {
-    drawPic ()
+function drawMatrix (matrix, format) {
+    let ctx = document.querySelector('.canvas').getContext("2d");
+    let scale = 512 / matrix.length;
+    let isRgba = format === 'rgba' ? true : false;
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (isRgba) ctx.fillStyle = `rgba(${matrix[i][j]})`;
+            else ctx.fillStyle = `#${matrix[i][j]}`;
+            ctx.fillRect(i*scale, j*scale, scale, scale);
+        }
+    }
+}
+
+document.querySelector('#img_lg').addEventListener('click', function () {
+    drawPic ();
+})
+document.querySelector('#img_sm').addEventListener('click', function () {
+    drawMatrix (matrix4 ,'#');
+})
+document.querySelector('#img_md').addEventListener('click', function () {
+    drawMatrix (matrix32, 'rgba');
 })
